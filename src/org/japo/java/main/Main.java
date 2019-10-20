4768d107794b48a27cc693dfa5ffc9739f4979ba
CicloM <CicloM@192.168.2.137>
2019-10-20 18:32:51 +0200

    :tada: Commit Inicial

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -15,15 +15,59 @@
  */
 package org.japo.java.main;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  *
  * @author Bianca Antonela Glavan - biancaantonela.glavan.alum@iescamp.es
  */
 public class Main {
+
     public static void main(String[] args) {
+
+        Calendar cal = Calendar.getInstance();
+
+        //Primera forma
         
-    
+        System.out.println("Hora del sistema.......: " + cal.get(cal.HOUR_OF_DAY) + ":" + cal.get(cal.MINUTE) + ":" + cal.get(cal.SECOND));
+        //d.getTime();
+
+        //segunda forma
         
+        //Variables 
+        //int h; //horas
+        //int m; //minutos
+        //int s; //segundos
+        //Objeto GregorianCalendar
+        //Calendar cal = Calendar.getInstance();
+        // Obtener valores
+        //System.out.println(Calendar.HOUR_OF_DAY); //VEr esta forma de la hora con las `posiciones.
+        //h = cal.get(Calendar.HOUR_OF_DAY); //su posicion es 11
+        //m = cal.get(Calendar.MINUTE);      //posicion 12 //m = cal.get(12);
+        //s = cal.get(Calendar.SECOND);      //posicion 13 // con estos valores me tendria que salir lo mismo que con poner "HOUR_OF_DAY, MINUTE Y SECOND)
+        //Mensaje
+        //System.out.printf("Hora del sistema.....args: %02d:%02d:%02d%n", h, m, s);
+        
+        //tercera forma
+        
+        System.out.printf("Hora del sistema.....args: %02d:%02d:%02d%n", cal.get(cal.HOUR_OF_DAY), cal.get(cal.MINUTE), cal.get(cal.SECOND));
+
+        //otra alternativa es utilziando Gregorian Calendar, es clase hija de Calendar
+        // Calendar cal = new GregorianCalendar();
+        //otra forma de sacar la hora pero con esta forma no puedo controlar los 4 digitos del año, solo salen 2
+        // final Locale LCL = new Locale("es", "ES");   //es-region ES -idioma
+        //final DateFormat DF = DateFormat.getDateInstance(DateFormat.LONG, LCL);
+        //Date d = new Date();
+        //System.out.println(DF.format(d));
+        //tercera forma
+        //public static final SimpleDateFormat SDF = new SimpleDateFormat();	
+        //public static final String PATRON_FECHA_CORTA = "dd/MM/yyyy"; ((( para poner en españos la fecha, new Locale("es", "ES");
+        //SDF.applyPattern(PATRON_FECHA_CORTA);
+        //Date d = new Date();
+        //System.out.println(SDF.format(cal.getTime()));
+        //otra alternatuva printf("%td/%tm/ty....
     }
-    
+
 }
